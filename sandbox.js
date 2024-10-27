26920bc53022adbe5573c7c3a5e101a35ca4a0d8
Harrison Wallace <hjbwallace@gmail.com>
2024-10-27 22:34:32 +0800

    Driving all data from the JS file

--- sandbox.js
+++ sandbox.js
@@ -1,8 +1,10 @@
-const imageContentBlock = {
-  image: "assets/banner.jpg",
-  heading: "Heading 123",
+const data = {
+  banner: "assets/banner.png",
+  image: "assets/image.jpg",
+  icon: "assets/icon.png",
+  heading: "Heading",
   text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  button: "Click Me 123",
+  button: "Click Me",
   imageRight: true,
 };
 
@@ -11,37 +13,39 @@ function initialise() {
 }
 
 function populateImageContentBlock() {
-  if (imageContentBlock.imageRight) {
+  if (data.imageRight) {
     document.getElementById('image-content-block').classList.add('image-content-block-right');
   }
 
-  populateElementSrc('image-content-block-image', imageContentBlock.image);
-  populateElementInnerText('image-content-block-content-heading', imageContentBlock.heading);
-  populateElementInnerText('image-content-block-content-text', imageContentBlock.text);
-  populateElementInnerText('image-content-block-content-button', imageContentBlock.button);
+  populateElementsInnerText('data-heading', data.heading);
+  populateElementsInnerText('data-text', data.text);
+  populateElementsInnerText('data-button', data.button);
+  populateElementsSrc('data-image', data.image);
+  populateElementsSrc('data-banner', data.banner);
+  populateElementsSrc('data-icon', data.icon);
+  populateElementsBackgroundImage('data-banner-background', data.banner);
 }
 
-function populateElementInnerText(elementId, content) { 
-  populateElementAttribute(elementId, 'innerText', content); 
+function populateElementsInnerText(elementId, content) { 
+  populateElementsAttribute(elementId, 'innerText', content); 
 }
 
-function populateElementSrc(elementId, content) { 
-  populateElementAttribute(elementId, 'src', content); 
+function populateElementsSrc(elementId, content) { 
+  populateElementsAttribute(elementId, 'src', content); 
 }
 
-function populateElementAttribute(elementId, attribute, content) {
-  var element = document.getElementById(elementId);
+function populateElementsBackgroundImage(elementId, content) { 
+  populateElementsAttribute(elementId, 'background-image', "url(" + content + ")", true); 
+}
 
-  if (!element) {
-    throw new Error("Element not found: " + elementId);
-  }
+function populateElementsAttribute(className, attribute, content, isStyle = false) {
+  var elements = document.getElementsByClassName(className);
 
-  if (!content || content === '') {
-    element.remove();
-    return;
-  }
+  var updateFunc = !content || content === ''
+    ? element => element.remove()
+    : element => isStyle ? element.style[attribute] = content : element[attribute] = content;
 
-  element[attribute] = content;
+  Array.prototype.forEach.call(elements, updateFunc);
 }
 
 window.onload = initialise;
